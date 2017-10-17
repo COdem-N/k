d632172be63d2e99125ecbe1996dde24d1aac11e
Carter <carterodem@gmail.com>
2017-10-17 16:07:18 -0700

    destroy the site

--- myscripts.js
+++ myscripts.js
@@ -1,7 +1,6 @@
-
 // See license: https://github.com/erkie/erkie.github.com/blob/master/README
 
-(function() {
+function game() {
 function Asteroids() {
 	if ( ! window.ASTEROIDS )
 		window.ASTEROIDS = {
@@ -9,7 +8,7 @@ function Asteroids() {
 			startedPlaying: (new Date()).getTime()
 		};
 
-	var BASEPATH = 'http://kickassapp.com/';
+	//var BASEPATH = 'http://kickassapp.com/';
 
 	/*
 		Classes
@@ -258,7 +257,7 @@ function Asteroids() {
 			  //self.container.style.webkitTransform = 'scale(1, 1)';
 			}, 50);
 
-			this.sendScore();
+			// this.sendScore();
 		},
 
 		hide: function() {
@@ -267,9 +266,9 @@ function Asteroids() {
 		},
 
 		sendScore: function() {
-		  var timePlayed = (new Date()).getTime() - window.ASTEROIDS.startedPlaying;
-			this.iframe.src = highscoreURL + "?asd=" + (window.ASTEROIDS.enemiesKilled * 10).toString() + "&sad=" + escape(document.location.href) + '&das=' + timePlayed;
-		}
+		//   var timePlayed = (new Date()).getTime() - window.ASTEROIDS.startedPlaying;
+		// 	this.iframe.src = highscoreURL + "?asd=" + (window.ASTEROIDS.enemiesKilled * 10).toString() + "&sad=" + escape(document.location.href) + '&das=' + timePlayed;
+	 }
 	};
 
 	/*
@@ -313,7 +312,7 @@ function Asteroids() {
 	var maxParticles = isIE ? 20 : 40;
 	var maxBullets = isIE ? 10 : 20;
 
-	var highscoreURL = BASEPATH + "highscores/";
+	//var highscoreURL = BASEPATH + "highscores/";
 
 	// generated every 10 ms
 	this.flame = {r: [], y: []};
@@ -597,17 +596,7 @@ function Asteroids() {
 		zIndex = "10000";
 	}
 
-	// Is IE
-	if ( typeof G_vmlCanvasManager != 'undefined' ) {
-		this.canvas = G_vmlCanvasManager.initElement(this.canvas);
-		if ( ! this.canvas.getContext ) {
-			alert("So... you're using IE?  Please join me at http://github.com/erkie/erkie.github.com if you think you can help");
-		}
-	} else {
-		if ( ! this.canvas.getContext ) {
-			alert('This program does not yet support your browser. Please join me at http://github.com/erkie/erkie.github.com if you think you can help');
-		}
-	}
+
 
 	addEvent(this.canvas, 'mousedown', function(e) {
 		e = e || window.event;
@@ -726,29 +715,23 @@ function Asteroids() {
 		this.highscoreLink.innerHTML = "Submit highscore";
 		this.navigation.appendChild(this.highscoreLink);
 
-		this.appstore = document.createElement('div');
-		with ( this.appstore.style ) {
-			position = 'fixed';
-			top = '10px';
-			right = '10px';
-			zIndex = '9999999';
-		}
-		this.appstore.className = 'ASTEROIDSYEAH';
-		this.appstore.innerHTML = '<a class="ASTEROIDSYEAH" target="_blank" href="http://itunes.apple.com/us/app/kick-ass-destroy-the-web/id436623109?mt=8&ls=1"><img src="http://erkie.github.com/appstore.png" class="ASTEROIDSYEAH" style="border: none" alt="Get the mobile version" /></a>';
-		this.appstore.getElementsByTagName('a')[0].onclick = function() {
-			this.parentNode.removeChild(this);
-		}
-		document.body.appendChild(this.appstore);
+		// this.appstore = document.createElement('div');
+		// with ( this.appstore.style ) {
+		// 	position = 'fixed';
+		// 	top = '10px';
+		// 	right = '10px';
+		// 	zIndex = '9999999';
+		// }
+		// this.appstore.className = 'ASTEROIDSYEAH';
+		// this.appstore.innerHTML = '<a class="ASTEROIDSYEAH" target="_blank" href="http://itunes.apple.com/us/app/kick-ass-destroy-the-web/id436623109?mt=8&ls=1"><img src="http://erkie.github.com/appstore.png" class="ASTEROIDSYEAH" style="border: none" alt="Get the mobile version" /></a>';
+		// this.appstore.getElementsByTagName('a')[0].onclick = function() {
+		// 	this.parentNode.removeChild(this);
+		// }
+		// document.body.appendChild(this.appstore);
 
 		// fb like box
-		this.fbLike = document.createElement('div');
-		this.fbLike.innerHTML = '<iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fpages%2FKick-Ass-Destroy-the-web%2F168200253236727&amp;width=292&amp;colorscheme=light&amp;show_faces=false&amp;stream=false&amp;header=false&amp;height=62" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:300px; height:70px;" allowTransparency="true"></iframe>';
-		this.navigation.appendChild(this.fbLike);
+		// this.fbLike = document.createElement('div');
 
-		// Don't show appstore on frontpage, because they are already present
-		if ( document.location.href === 'http://erkie.github.com/' ) {
-			this.appstore.style.display = "none";
-		}
 
 		this.highscoreLink.onclick = function() {
 			if ( ! that.highscores ) {
@@ -1214,14 +1197,6 @@ if ( window.ActiveXObject && ! document.createElement('canvas').getContext ) {
 }
 else window.ASTEROIDSPLAYERS[window.ASTEROIDSPLAYERS.length] = new Asteroids();
 
-var trackingFrame = document.createElement('iframe');
-trackingFrame.src = 'http://erkie.github.com/tracking.html';
-trackingFrame.frameborder = '0';
-trackingFrame.style.position = 'absolute';
-trackingFrame.style.top = "-1000px";
-trackingFrame.style.height = "0px";
-trackingFrame.style.width = "0px";
 
-document.getElementsByTagName('body')[0].appendChild(trackingFrame);
 
-})();
+}
