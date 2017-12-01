3f87ab2ed33c99fece4addb4b9785ffcd9f92cdc
merka <merka@uw.edu>
2017-11-30 18:19:59 -0800

    changed gui creation in main

--- model/Main.java
+++ model/Main.java
@@ -1,12 +1,19 @@
 package model;
 
+import java.awt.EventQueue;
+
 import view.MainGUI;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new MainGUI();
+		EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new MainGUI();
+            }
+        });
 	}
 
 }
