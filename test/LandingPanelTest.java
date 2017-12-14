07d98d5499979a0e3a8d18ff62536c296c1d45eb
carter <carterodem@gmail.com>
2017-12-14 12:03:18 -0800

    comments on test

--- test/LandingPanelTest.java
+++ test/LandingPanelTest.java
@@ -12,13 +12,23 @@ import view.MainGUI;
 
 public class LandingPanelTest {
 
+	
+    /**
+     * a test for the constructor to make sure it creates an object.
+     * 
+     * @author Carter Odem
+     */
 	@Test
 	public void testLandingPanel() {
 		LandingPanel landPanel = new LandingPanel();
 
 		assertNotNull(landPanel);
 	}
-
+    /**
+     *  a test to make sure that the move to category. doesn't remove the previous panel
+     * 
+     * @author Carter Odem
+     */
 	@Test
 	public void testMoveToCategory() {
 		final JFrame frame = new JFrame("");
@@ -29,7 +39,11 @@ public class LandingPanelTest {
 		landPanel.moveToCategory();
 		assertNotNull(landPanel);
 	}
-
+    /**
+     * a test of the setup method to make sure that it can create the GUI properly
+     * 
+     * @author Carter Odem
+     */
 	@Test
 	public void testSetup() {
 		MainGUI muGui = new MainGUI();
